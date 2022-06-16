5769c6c2fd28d14aea8290405d3d0409c17af4b8
mingsonzheng <mingsonzheng@outlook.com>
2022-06-16 17:29:42 +0800

    001search

---
+++ 007BinarySearch/001search/001search.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	target := 9
+	fmt.Println(search(nums, target))
+	nums = []int{-1, 0, 3, 5, 9, 12}
+	target = 2
+	fmt.Println(search(nums, target))
+}
+
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
