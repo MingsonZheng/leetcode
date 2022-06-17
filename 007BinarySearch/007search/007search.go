07ab801b807c30dd06ab6353b3c36da0258d3600
mingsonzheng <mingsonzheng@outlook.com>
2022-06-17 10:59:12 +0800

    007search

---
+++ 007BinarySearch/007search/007search.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// 33. 搜索旋转排序数组
+
+func main() {
+	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	target := 0
+	fmt.Println(search(nums, target))
+	target = 3
+	fmt.Println(search(nums, target))
+	nums = []int{1}
+	target = 0
+	fmt.Println(search(nums, target))
+}
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[left] <= nums[mid] { // left side sorted
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
