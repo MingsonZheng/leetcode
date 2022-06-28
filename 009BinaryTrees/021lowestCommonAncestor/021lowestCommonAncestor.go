f7a34bfe7b6ccc694bf19426c14b5f4afd59417e
mingsonzheng <mingsonzheng@outlook.com>
2022-06-28 11:49:26 +0800

    lowestCommonAncestor

--- 009BinaryTrees/021lowestCommonAncestor/021lowestCommonAncestor.go
+++ 009BinaryTrees/021lowestCommonAncestor/021lowestCommonAncestor.go
@@ -1,11 +1,47 @@
 package main
 
+import "fmt"
+
 // 236. 二叉树的最近公共祖先
 
 func main() {
-
+	array := []int{3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4}
+	root := MakeTree(array)
+	p := root.Left
+	q := root.Right
+	fmt.Println(PreorderTraversal(root))
+	root = lowestCommonAncestor(root, p, q)
+	fmt.Println(PreorderTraversal(root))
 }
 
-//func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-//
-//}
+var lca *TreeNode
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	lca = nil
+	traverse(root, p, q)
+	return lca
+}
+func traverse(root, p, q *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftContains := traverse(root.Left, p, q)
+	if lca != nil { // 提前退出
+		return 2
+	}
+	rightContains := traverse(root.Right, p, q)
+	if lca != nil { // 提前退出
+		return 2
+	}
+	rootContains := 0
+	if root == p || root == q {
+		rootContains = 1
+	}
+	if rootContains == 0 && leftContains == 1 && rightContains == 1 {
+		lca = root
+	}
+	if rootContains == 1 && (leftContains == 1 || rightContains == 1) {
+		lca = root
+	}
+	return leftContains + rightContains + rootContains
+}
